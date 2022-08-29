33d7c6f7c2305c896d08d6384a0811de453c6105
Amila Senadheera <amila.15@cse.mrt.ac.lk>
2022-08-29 23:26:46 +0530

    update filter to use as http 11 proxy

--- http-filter-example/http_filter.cc
+++ http-filter-example/http_filter.cc
@@ -4,6 +4,10 @@
 
 #include "envoy/server/filter_config.h"
 
+#include "source/common/network/filter_state_proxy_info.h"
+#include "source/common/network/utility.h"
+#include "source/extensions/filters/http/common/pass_through_filter.h"
+
 namespace Envoy {
 namespace Http {
 
@@ -26,10 +30,22 @@ const std::string HttpSampleDecoderFilter::headerValue() const {
   return config_->val();
 }
 
-FilterHeadersStatus HttpSampleDecoderFilter::decodeHeaders(RequestHeaderMap& headers, bool) {
+FilterHeadersStatus HttpSampleDecoderFilter::decodeHeaders(RequestHeaderMap& request_headers, bool) {
   // add a header
-  headers.addCopy(headerKey(), headerValue());
-
+  request_headers.addCopy(headerKey(), headerValue());
+  auto connect_proxy = LowerCaseString("connect-proxy");
+  auto hostname = request_headers.getHostValue();
+  ASSERT(!hostname.empty());
+  if (!request_headers.get(connect_proxy).empty()) {
+    std::string address_string(request_headers.get(connect_proxy)[0]->value().getStringView());
+    auto address = Network::Utility::parseInternetAddressAndPort(address_string);
+    decoder_callbacks_->streamInfo().filterState()->setData(
+        Network::Http11ProxyInfoFilterState::key(),
+        std::make_unique<Network::Http11ProxyInfoFilterState>(hostname, address),
+        StreamInfo::FilterState::StateType::ReadOnly,
+        StreamInfo::FilterState::LifeSpan::FilterChain);
+    request_headers.remove(connect_proxy);
+  }
   return FilterHeadersStatus::Continue;
 }
 
